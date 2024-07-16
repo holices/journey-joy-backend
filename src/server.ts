d9ee468a3bfe6441ff24ed4a63df94f897829054
Holices Junior <dev.holices@gmail.com>
2024-07-16 22:39:48 +0100

    added routes + route management

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,20 @@
 import fastify from "fastify";
-import { prisma } from "./lib/prisma";
+import cors from "@fastify/cors";
+import { createTrip } from "./routes/create-trips";
+import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import { confirmTrip } from "./routes/confirm-trip";
 
 const app = fastify()
 
-app.get('/test', () => {
-  return 'hello world' 
+app.register(cors, {
+  origin: 'http://localhost:3333'
 })
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+app.register(confirmTrip)
+
+app.register(createTrip)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Server running')
