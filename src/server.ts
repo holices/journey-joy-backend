e92b00442c07b44ec5052efa409fae14b74d93e0
Holices Junior <dev.holices@gmail.com>
2024-07-20 21:48:30 +0100

    participants confirm route added

--- src/server.ts
+++ src/server.ts
@@ -3,18 +3,20 @@ import cors from "@fastify/cors";
 import { createTrip } from "./routes/create-trips";
 import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
 import { confirmTrip } from "./routes/confirm-trip";
+import { confirmParticipants } from "./routes/confirm-participant";
 
 const app = fastify()
 
 app.register(cors, {
-  origin: 'http://localhost:3333'
+  origin: 'http://localhost:3333' 
 })
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
-app.register(confirmTrip)
 
 app.register(createTrip)
+app.register(confirmTrip)
+app.register(confirmParticipants)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Server running')
