01f3e2019c63b7aca7ec841c8e50914f217aad07
Holices Junior <dev.holices@gmail.com>
2024-07-21 20:10:30 +0100

    routes added

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,15 @@ import { createTrip } from "./routes/create-trips";
 import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
 import { confirmTrip } from "./routes/confirm-trip";
 import { confirmParticipants } from "./routes/confirm-participant";
+import { createActivity } from "./routes/create-activity";
+import { getActivities } from "./routes/get-activities";
+import { createLink } from "./routes/create-link";
+import { getLinks } from "./routes/get-links";
+import { createInvite } from "./routes/create-invite";
+import { getParticipants } from "./routes/get-participants";
+import { updateTrip } from "./routes/update-trip";
+import { getTripDetails } from "./routes/get-trip-details";
+import { getParticipant } from "./routes/get-participant";
 
 const app = fastify()
 
@@ -17,6 +26,15 @@ app.setSerializerCompiler(serializerCompiler)
 app.register(createTrip)
 app.register(confirmTrip)
 app.register(confirmParticipants)
+app.register(createActivity)
+app.register(getActivities)
+app.register(createLink)
+app.register(getLinks)
+app.register(getParticipants)
+app.register(createInvite)
+app.register(updateTrip)
+app.register(getTripDetails)
+app.register(getParticipant)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Server running')
